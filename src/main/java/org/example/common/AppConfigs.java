4e441c85f8c11d5c3413710800c1268edea32fbf
deshbandhu <deshbandhu.mishra@gmail.com>
2020-06-30 09:58:54 +0530

    Initial commit

---
+++ src/main/java/org/example/common/AppConfigs.java
@@ -0,0 +1,9 @@
+package org.example.common;
+
+public class AppConfigs {
+   // public final static String applicationID = "HelloProducer";
+    //public final static String bootstrapServers = "localhost:9092";//,localhost:9093";
+    //public final static String bootstrapServers = "0.0.0.0:9092,localhost:9092";
+    //public final static String topicName = "hello-producer";
+   // public final static int numEvents = 10;
+}
